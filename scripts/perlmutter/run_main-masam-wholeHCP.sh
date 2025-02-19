2d64c738b0a6964a7fe365fb80f7647fb7a9c26d
Junebeomstics <kjb961013@naver.com>
2025-02-18 21:47:59 -0800

    updates

---
+++ scripts/perlmutter/run_main-masam-wholeHCP.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -A m4727_g
+#SBATCH -C gpu
+#SBATCH -q regular
+#SBATCH -t 24:00:00
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH -c 128
+#SBATCH --exclusive
+#SBATCH --output=R-%x-%j.out
+#SBATCH --mail-user=kjb961013@snu.ac.kr
+#SBATCH --chdir=/global/cfs/cdirs/m4673/junbeom/TractSegVis/TractSeg/
+module load pytorch/2.1.0
+
+cd /global/cfs/cdirs/m4673/junbeom/TractSegVis/TractSeg/
+./ExpRunner --config wholeHCP_experiment_MASAM --en run_with_wholeHCP_MASAM --train True --test True --seg 
+#./ExpRunner --config wholevisualtract_experiment --en run_with_vis_settings --train True --test True
---
+++ scripts/perlmutter/run_main_masam.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l 
+#SBATCH -A m4727_g
+#SBATCH -C gpu
+#SBATCH -q regular
+#SBATCH -t 24:00:00
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH -c 128
+#SBATCH --exclusive
+#SBATCH --output=R-%x-%j.out
+#SBATCH --mail-user=kjb961013@snu.ac.kr
+#SBATCH --chdir=/global/cfs/cdirs/m4673/junbeom/TractSegVis/TractSeg/
+
+module load pytorch/2.1.0
+# pip install icecream
+# pip install triton-2.2.0
+
+cd /global/cfs/cdirs/m4673/junbeom/TractSegVis/TractSeg/
+
+# bigger model (vit_l) -> using pretrained patch embedding layer with new conv1d layer
+./ExpRunner --config wholeHCP_experiment_MASAM --en run_with_wholeHCP_MASAM --train True --test True --seg --lw 
+#./ExpRunner --config wholevisualtract_experiment --en run_with_vis_settings --train True --test True
